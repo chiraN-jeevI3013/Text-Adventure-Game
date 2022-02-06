43b993d7765f0a60c64275288dc641583024513a
chiraN-jeevI3013 <87750639+chiraN-jeevI3013@users.noreply.github.com>
2022-02-07 00:12:58 +0530

    Create Gameplay.java

---
+++ CLI/Gameplay.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Gameplay
+{
+	public static Chapter0 c0 = new Chapter0();
+	
+	public static void main(String[] args) throws InputMismatchException, Exception
+	{
+		c0.mod1();
+		
+	}
+
+}
